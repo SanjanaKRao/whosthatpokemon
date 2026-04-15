de3319e589066bb9df6839838e34f556710e0f8f
Sanjana K Rao <sanjanakrao@Sanjanas-Laptop-2.local>
2026-04-15 14:44:02 +0530

    Added GA

--- js/tracking.js
+++ js/tracking.js
@@ -1,1 +1,28 @@
-/* Placeholder for optional tracking hooks. */
+(function () {
+    const measurementId = window.GA_MEASUREMENT_ID;
+
+    window.tracking = {
+        enabled: Boolean(measurementId),
+        track(eventName, params = {}) {
+            if (!measurementId || typeof window.gtag !== 'function') {
+                return;
+            }
+
+            window.gtag('event', eventName, params);
+        }
+    };
+
+    if (!measurementId) {
+        return;
+    }
+
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = window.gtag || function gtag() {
+        window.dataLayer.push(arguments);
+    };
+
+    window.gtag('js', new Date());
+    window.gtag('config', measurementId, {
+        send_page_view: true
+    });
+})();
